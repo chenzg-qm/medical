4272889eb5f9fb8b6b5573dc607f5360957df42b
chenzg-qm <chenzg_qm@hotmail.com>
2015-04-05 15:33:34 +0800

    add db script pack.

---
+++ code/s3d/medical/src/main/java/com/s3d/webapps/web/DD.java
@@ -0,0 +1,12 @@
+package com.s3d.webapps.web;
+
+import com.test.xxx.ddd.Xyz;
+
+/**
+ * Created by Administrator on 2015/4/5.
+ */
+public class DD {
+    public void rn(){
+        Xyz xy =new Xyz();
+    }
+}
